83e71b5ad6c2e2986763c64a68d47edc6e85a8da
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-26 20:06:47 -0600

    added mysql into index.js

--- Main/routes/index.js
+++ Main/routes/index.js
@@ -10,4 +10,14 @@ router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Starwars0819',
+    database: 'ecommerce_db'
+  },
+  console.log(`Connected to the ecommerce_db database.`)
+);
+
 module.exports = router;
